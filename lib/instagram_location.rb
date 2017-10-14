808eaae956bbe4d70ffc00ce88374c5c9efb2215
Jack Lee <cp000123@gmail.com>
2017-10-14 16:52:20 +0800

    result, response success

--- lib/instagram_location.rb
+++ lib/instagram_location.rb
@@ -3,8 +3,10 @@ require 'yaml'
 require 'json'
 config = YAML::safe_load(File.read('config/secret.yml'))
 
+tmp = {}
 result = {}
-
+response = {}
+i = 0
 def ig_path(token, location)
 	"https://api.instagram.com/v1/locations/"+location+"/media/recent?access_token="+token
 end
@@ -19,8 +21,17 @@ location_id = '715635978608753'
 
 path = ig_path(config,location_id)
 
-result = call_url(path)
+response = call_url(path)
+tmp = JSON.parse(response)
 
-result_parse = JSON.parse(result)
-result_yml = result_parse.to_yaml
-puts result_yml
+#stupid way
+tmp['data'].each do |id|
+  result[i] = id['images']['standard_resolution']['url']
+  i+=1
+end
+
+#puts result
+#result_yml = result_parse.to_yaml
+#puts result_yml
+File.write('../spec/fixtures/response.yml', response.to_yaml)
+File.write('../spec/fixtures/result.yml', result.to_yaml)
